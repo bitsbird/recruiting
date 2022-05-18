d18930761be91b6170513b4e3d6bf3e09ec7a98c
emanuele ci <bitsbird@users.noreply.github.com>
2022-05-18 16:10:59 +0200

    Create warmup.js

---
+++ warmup.js
@@ -0,0 +1,13 @@
+// print all available filters
+
+"use strict";
+ 
+const filters = [
+   [{ name: "term", value: "golf" }],
+   [{ name: "price_from", value: "100" }],
+   [{ name: "category", value: "cars" }, { name: "model", value: "bmw" }],
+   [{ name: "category", value: "job" }, { name: "title", value: "engineer" }],
+   [{ name: "term", value: "t-shirt" }],
+   [{ name: "category", value: "clothes"}, { name: "price_from", value: "20"}]
+];
+
